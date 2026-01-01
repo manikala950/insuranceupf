34e4c18ad7c6abe81f6cb5c69679eea57a106e52
manikala950 <manikalasubramanyam5@gmail.com>
2026-01-01 12:14:28 +0530

    logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -27,7 +27,7 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
           <NavLink to="/" className="flex items-center space-x-3">
-            <img src={logo} alt="UPLIFE India" className="h-12 w-auto" />
+            <img src={logo} alt="UPLIFE India" className="h-20 w-100" />
           </NavLink>
 
           {/* Desktop Navigation */}
